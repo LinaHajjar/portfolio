90c86812345740ef09b7abb6e42824e2e4446115
Lina Al-Hajjar <lial0001@stud.kea.dk>
2025-08-01 12:27:53 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+document.getElementById("toggleInfo").addEventListener("click", function() {
+    const info = document.getElementById("extraInfo");
+
+    // Toggle klassen "hidden" af eller på
+    info.classList.toggle("hidden");
+
+    // Skift tekst på knappen
+    if (info.classList.contains("hidden")) {
+        this.textContent = "Vis mere";
+    } else {
+        this.textContent = "Skjul";
+    }
+});
